f6457d0a762c21bd33895c156e2181ab72117471
Shelby Polasek <shelbypolasek@gmail.com>
2020-09-25 15:27:26 -0500

    jumbotron working with reactstrap

--- client/src/JumbotronStrap.js
+++ client/src/JumbotronStrap.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import {Jumbotron, Container} from 'reactstrap';
-
-export class JumbotronStrap extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <Jumbotron fluid>
-                    <Container fluid>
-                        <h1 className="display-3">Fluid jumbotron</h1>
-                        <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of
-                            its parent.</p>
-                    </Container>
-                </Jumbotron>
-            </div>
-        );
-    }
-}
-
-export default JumbotronStrap;
+// import React from 'react';
+// import {Jumbotron, Container} from 'reactstrap';
+//
+// export class JumbotronStrap extends React.Component {
+//
+//     render() {
+//         return (
+//             <div>
+//                 <Jumbotron fluid>
+//                     <Container fluid>
+//                         <h1 className="display-3">Fluid jumbotron</h1>
+//                         <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of
+//                             its parent.</p>
+//                     </Container>
+//                 </Jumbotron>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default JumbotronStrap;
 
--- client/src/RandButton.js
+++ client/src/RandButton.js
@@ -8,6 +8,12 @@ export class RandButton extends React.Component{
         this.handleRandNum = this.handleRandNum.bind(this);
     }
 
+    // constructor(props){
+    //     super(props);
+    //     this.state = { hired: 'not a chance' };
+    //     this.handleHired = this.handleHired.bind(this);
+    // }
+
     handleRandNum() {
         this.setState(
             {randNum : Math.round(Math.random() * 10)}
